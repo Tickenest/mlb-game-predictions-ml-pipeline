c3d7c5458161608b45340eea0e351202999f7bb7
Tickenest <tickenest@gmail.com>
2026-05-03 11:37:08 -0400

    Add daily prediction schedule at 12:30 PM ET, generate May 3 predictions

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -18,3 +18,23 @@ resource "aws_lambda_permission" "eventbridge_ingestion" {
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.daily_ingestion.arn
 }
+
+resource "aws_cloudwatch_event_rule" "daily_prediction" {
+  name                = "${var.project_name}-daily-prediction"
+  description         = "Triggers MLB prediction generation daily at 1 PM ET"
+  schedule_expression = "cron(30 16 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "daily_prediction" {
+  rule      = aws_cloudwatch_event_rule.daily_prediction.name
+  target_id = "prediction-lambda"
+  arn       = aws_lambda_function.prediction.arn
+}
+
+resource "aws_lambda_permission" "eventbridge_prediction" {
+  statement_id  = "AllowEventBridgePrediction"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.prediction.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.daily_prediction.arn
+}
